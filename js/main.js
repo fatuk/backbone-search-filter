4b87afcde71f83bff3acd15bd9fff4b0e688477e
fatuk <fatukk@gmail.com>
2014-06-10 23:57:50 +0400

    Add search

--- js/main.js
+++ js/main.js
@@ -1,30 +1,77 @@
-$(function () {
-	window.App = {};
-	App.Views = {};
-	App.Models = {};
-	App.Collections = {};
+$(function() {
+    window.App = {};
+    App.Views = {};
+    App.Models = {};
+    App.Collections = {};
 
-	var persons = [];
+    var test = {
+        name: 'Andrew'
+    };
 
-	for (var i = 0; i < 100; i++) {
-		persons.push(Faker.Helpers.userCard());
-		persons[i].avatar = Faker.random.avatar_uri();
-	}
+    console.log(_(test));
 
 
-	App.Views.App = Backbone.View.extend({
-		el: '#app'
-	});
+    App.Views.App = Backbone.View.extend({
+        el: '#app',
+        events: {
+            'blur #searchInput': 'search'
+        },
+        initialize: function() {
+            this.renderSorted(this.collection);
+        },
+        renderSorted: function(sorted) {
+            this.searchCount = sorted._wrapped ? sorted._wrapped.length : sorted.length;
+            this.$('#persons').html('');
+            sorted.each(function(person) {
+                var personView = new App.Views.Person({
+                    model: person
+                });
+                this.$('#persons').append(personView.el);
+            }, this);
 
-	App.Views.List = Backbone.View.extend({
+            this.$('.js-searchCount').html(this.searchCount);
+        },
+        search: function(e) {
+            var request = $(e.target).val();
+            this.renderSorted(this.collection.search(request));
+        }
+    });
 
-	});
+    App.Views.Person = Backbone.View.extend({
+        tagName: 'li',
+        className: 'person__item',
+        template: $('#personTemplate').html(),
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            var rendered = Mustache.render(this.template, this.model.toJSON());
+            this.$el.append(rendered);
+            return this;
+        }
+    });
 
-	App.Collections.Books = Backbone.View.extend({
-		url: 'data/persons.json'
-	});
+    App.Collections.Persons = Backbone.Collection.extend({
+        initialize: function() {
+            var persons = this;
+            for (var i = 0; i < 1000; i++) {
+                persons.add(Faker.Helpers.userCard());
+                persons.models[i].set('avatar', Faker.random.avatar_uri());
+            }
+        },
+        search: function(letters) {
+            if (letters === "") return this;
 
-	App.Collections.Books = Backbone.View.extend({
-		url: 'data/persons.json'
-	});
+            var pattern = new RegExp(letters, "gi");
+            return _(this.filter(function(data) {
+                return pattern.test(data.get("username"));
+            }));
+        }
+    });
+
+
+    var persons = new App.Collections.Persons();
+    var appView = new App.Views.App({
+        collection: persons
+    });
 });
